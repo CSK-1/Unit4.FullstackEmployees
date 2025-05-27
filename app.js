6686da3fa12d5e7d9372f2768322a6230b196734
Chase K <chasekmbll@gmail.com>
2025-05-26 21:48:59 -0500

    Initial commit for part 2 of workshop

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 import express from "express";
 const app = express();
 export default app;
+import employeesRouter from "./api/employees.js"
 
-// TODO: this file!
+app.use(express.json());
+
+app.route("/").get((req, res) => {
+    response.send("Welcome to the Fullstack Employees API.");
+  });
+
+app.use("/employees", employeesRouter);
+
+app.use((err, req, res, next) => {
+	console.error(err);
+	res.status(500).send("Sorry! Something went wrong :(");
+});
--- db/queries/employees.js
+++ db/queries/employees.js
@@ -13,7 +13,13 @@ export async function createEmployee({ name, birthday, salary }) {
 
 /** @returns all employees */
 export async function getEmployees() {
-  // TODO
+  const sql = `
+  	SELECT *
+  	FROM employees;
+ 	 `;
+	const { rows: employees } = await db.query(sql);
+
+	return employees;
 }
 
 /**
@@ -21,7 +27,11 @@ export async function getEmployees() {
  * @returns undefined if employee with the given id does not exist
  */
 export async function getEmployee(id) {
-  // TODO
+  const sql = `
+	SELECT * FROM employees WHERE id = $1;
+	`;
+	const { rows: employee } = await db.query(sql, [id]);
+	return employee[0];
 }
 
 /**
@@ -29,7 +39,19 @@ export async function getEmployee(id) {
  * @returns undefined if employee with the given id does not exist
  */
 export async function updateEmployee({ id, name, birthday, salary }) {
-  // TODO
+  const sql = `
+	UPDATE employees
+	SET name = $1, birthday = $2, salary = $3
+	WHERE id = $4
+	RETURNING *;
+	`;
+	const { rows: employee } = await db.query(sql, [
+		name,
+		birthday,
+		salary,
+		id,
+	]);
+	return employee[0];
 }
 
 /**
@@ -37,5 +59,9 @@ export async function updateEmployee({ id, name, birthday, salary }) {
  * @returns undefined if employee with the given id does not exist
  */
 export async function deleteEmployee(id) {
-  // TODO
+  const sql = `
+	DELETE FROM employees WHERE id = $1 RETURNING *;
+	`;
+	const { rows: employee } = await db.query(sql, [id]);
+	return employee;
 }
